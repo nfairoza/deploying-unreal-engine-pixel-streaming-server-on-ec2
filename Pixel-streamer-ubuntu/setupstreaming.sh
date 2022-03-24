61780f9ab062b24a0408052133e32244492d46b8
EC2 Default User <ec2-user@ip-172-31-79-243.ec2.internal>
2022-03-24 00:32:23 +0000

    added script

--- Pixel-streamer-ubuntu/setupstreaming.sh
+++ Pixel-streamer-ubuntu/setupstreaming.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt update -y
 sudo echo "ubuntu:password" | sudo chpasswd
+cd /home/ubuntu/Documents/PackagedProject/LinuxNoEditor/
+nohup ./fps.sh -RenderOffScreen -PixelStreamingIP=127.0.0.1 -PixelStreamingPort=8888 > /dev/null 2>&1 &
 cd /home/ubuntu/Desktop/UnrealEngine/Samples/PixelStreaming/WebServers/SignallingWebServer/platform_scripts/bash
 ./setup.sh
 nohup ./Start_SignallingServer.sh & disown
-cd /home/ubuntu/Documents/PackagedProject/LinuxNoEditor/
-screen -S screensession ./fps.sh -RenderOffScreen -PixelStreamingIP=127.0.0.1 -PixelStreamingPort=8888 & disown
 pwd
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
